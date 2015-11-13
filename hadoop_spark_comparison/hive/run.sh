06d3352859b479e81ce91ea6bcc5a41008e146aa
Ubuntu <ubuntu@ip-172-31-5-218.us-west-2.compute.internal>
2015-11-13 22:57:11 +0000

    added timing for spark and hive. Still having error for hive write out to hdfs

--- hadoop_spark_comparison/hive/run.sh
+++ hadoop_spark_comparison/hive/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-hive -f reddit_data.sql --hiveconf fs.s3n.awsAccessKeyId=$AWS_ACCESS_KEY_ID --hiveconf fs.s3n.awsSecretAccessKey=$AWS_SECRET_ACCESS_KEY --hiveconf mapreduce.application.classpath=$HADOOP_HOME/share/hadoop/mapreduce/*:$HADOOP_HOME/share/hadoop/mapreduce/lib/*:$HADOOP_HOME/share/hadoop/tools/lib/* --hiveconf fs.s3n.impl=org.apache.hadoop.fs.s3native.NativeS3FileSystem
+time hive -f reddit_data.sql --hiveconf fs.s3n.awsAccessKeyId=$AWS_ACCESS_KEY_ID --hiveconf fs.s3n.awsSecretAccessKey=$AWS_SECRET_ACCESS_KEY --hiveconf mapreduce.application.classpath=$HADOOP_HOME/share/hadoop/mapreduce/*:$HADOOP_HOME/share/hadoop/mapreduce/lib/*:$HADOOP_HOME/share/hadoop/tools/lib/* --hiveconf fs.s3n.impl=org.apache.hadoop.fs.s3native.NativeS3FileSystem
--- hadoop_spark_comparison/spark_df/run.sh
+++ hadoop_spark_comparison/spark_df/run.sh
@@ -2,4 +2,4 @@
 
 hdfs dfs -rm -r /spark-output
 
-spark-submit --master spark://ip-172-31-4-108:7077 --executor-memory 6100M --driver-memory 6100M --packages com.databricks:spark-csv_2.10:1.1.0 reddit_data.py
+time spark-submit --master spark://$(hostname):7077 --executor-memory 6100M --driver-memory 6100M --packages com.databricks:spark-csv_2.10:1.1.0 reddit_data.py
